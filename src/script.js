d20bd7b1b5a2116a7a60493f3402503c999c9611
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2020-10-27 12:44:19 +1300

    finished JS

--- src/script.js
+++ src/script.js
@@ -24,6 +24,11 @@ function addTransaction(e) {
             amount: +amount.value
         }
         transactions.push(transaction)
+
+        addTransactionList(transaction)
+
+        updateValues()
+        updateLocalStorage()
     }
 }
 
@@ -37,7 +42,56 @@ function generateId() { // Math.floor rounds the number, Math.random gives a ran
 
 function addTransactionList(transaction) {
     // Get sign
-    const sign = transaction < 0 ? '-' : '+'
+    const sign = transaction.amount < 0 ? '-' : '+'
 
     const item = document.createElement('li') 
+
+    // Add a class based on the value of amount
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus')
+
+    item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" onclick="removeItem(${transaction.id})">x</button>
+    `
+
+    list.appendChild(item)
 }
+
+// Update total card
+function updateValues() {
+    const amount = transactions.map(transaction => transaction.amount)
+    
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+
+    const income = amounts.filter(item => item > 0).reduce((acc, itme) => (acc += item), 0).toFixed(2)
+
+    const expense = amounts.filter(item => item < 0).reduce(((acc, item) => (acc += item), 0) * -1).toFIxed(2)
+
+    balance.innerText = `$${total}`
+    money_plus.innerText = `$${income}`
+    money_minus.innerText = `$${expense}`
+}
+
+// Remove item by id
+function removeItem(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id)
+
+    updateLocalStorage()
+}
+
+// Update local storage
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+}
+
+// init app
+function init() {
+    list.innerHTML = ''
+
+    transactions.forEach(addTransactionList) 
+
+    updateValues()
+}
+
+// Add transaction
+form.addEventListener('submit', addTransaction)
